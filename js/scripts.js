f40f0ff87df89e15aabaa51d83f02fba8d96275e
xintamosaik <xintamosaik@gmail.com>
2022-05-22 20:53:18 +0200

    overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,32 @@ const init = () => {
         document.body.classList.toggle("light")
     })
 
+    const toggleClassTwiceAfterDelay = (elementID, milliseconds) => {
+        const targetedElement = document.getElementById(elementID)
+        targetedElement.classList.toggle('arrivedByOnsiteLink')
+        setTimeout(() => {
+            targetedElement.classList.toggle('arrivedByOnsiteLink')
+        }, milliseconds)
+     }
+
+    const onsiteLinks = document.querySelectorAll('a.onsite')
+    onsiteLinks.forEach( onsitelink => {
+        onsitelink.addEventListener('click', event => {
+            const linkReference = event.currentTarget.href
+            const onsiteTargetElementID = linkReference.split('#')[1]
+            toggleClassTwiceAfterDelay( onsiteTargetElementID , 5000 ) 
+        })
+    })
+
+    const  onInputInMainTextArea = targetedTextArea => {
+          targetedTextArea.style.height = "auto";
+          targetedTextArea.style.height = (targetedTextArea.scrollHeight) + "px";
+    }
+    const mainTextarea = document.getElementById('mainTextarea')
+    mainTextarea.addEventListener("input", event  => {
+        onInputInMainTextArea(event.target)
+    })
 }
 
+console.log('scripts.js:')
 document.addEventListener("DOMContentLoaded", init);
